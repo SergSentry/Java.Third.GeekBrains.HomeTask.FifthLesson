59d2dfa105ae5dfe747c8e2c4f25bafdedc1648f
Serg <rtfm.faq@mail.ru>
2018-10-02 00:36:46 +0300

    FifthLesson: After change

--- src/Car.java
+++ src/Car.java
@@ -1,11 +1,19 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     static {
         CARS_COUNT = 0;
     }
-    
+
     private Race race;
     private int speed;
+    private CyclicBarrier pistol;
+    private CountDownLatch beginRace;
+    private CountDownLatch endRace;
+    private AtomicBoolean finishLine;
     private String name;
 
     public String getName() {
@@ -16,9 +24,13 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier pistol, CountDownLatch beginRace, CountDownLatch endRace, AtomicBoolean finishLine) {
         this.race = race;
         this.speed = speed;
+        this.pistol = pistol;
+        this.beginRace = beginRace;
+        this.endRace = endRace;
+        this.finishLine = finishLine;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
@@ -29,11 +41,22 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+
+            beginRace.countDown();
+            pistol.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+
+        if (!finishLine.getAndSet(true))
+        {
+            System.out.printf("Участник #%d WIN\n",CARS_COUNT);
+        }
+
+        endRace.countDown();
     }
 }
--- src/Program.java
+++ src/Program.java
@@ -1,22 +1,37 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Program {
     public static final int CARS_COUNT = 4;
+    private static final int CARS_HALF_COUNT = CARS_COUNT / 2;
 
-    public static void main(String[] args) {
+    private static Semaphore bottleneck = new Semaphore(CARS_HALF_COUNT);
+    private static CountDownLatch beginRace = new CountDownLatch(CARS_COUNT);
+    private static CountDownLatch endRace = new CountDownLatch(CARS_COUNT);
+    private static CyclicBarrier pistol = new CyclicBarrier(CARS_COUNT);
+
+    private static AtomicBoolean finishLine = new AtomicBoolean(false);
+
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
 
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Race race = new Race(new Road(60), new Tunnel(bottleneck), new Road(40));
 
         Car[] cars = new Car[CARS_COUNT];
 
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), pistol, beginRace, endRace, finishLine);
         }
 
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
 
+        beginRace.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        endRace.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
